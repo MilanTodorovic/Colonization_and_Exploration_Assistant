266aac438afed5008e1109540763c2a37238c8be
Milan Todorovic <todor-90@hotmail.com>
2020-09-24 22:41:29 +0200

    Experimenting with layouts. Created 5 template Scenes and Controllers.

--- src/core/Main.java
+++ src/core/Main.java
@@ -4,20 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("layout/mainScene.fxml"));
         primaryStage.setTitle("Colonization and Exploration Assistant v0.1");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Scene mainScene = new Scene(root, 300, 300);
+
+        Pane rootPane = new Pane();
+
+        Pane mapView = new Pane();
+        Pane systemView = new Pane();
+        Pane planetView = new Pane();
+        Pane filterView = new Pane();
+
+        rootPane.getChildren().addAll(mapView,systemView,planetView, filterView);
+
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
---
+++ src/core/layout/MainSceneController.java
@@ -0,0 +1,18 @@
+package core.layout;
+
+import javafx.scene.control.*;
+
+
+public class MainSceneController {
+
+    public Button btn;
+
+    public void handleButtonClick() {
+        btn.setText("Promenili smo tekst");
+    }
+
+    //    <fx:include source="planetView.fxml"/>
+    //    <fx:include source="mapView.fxml"/>
+    //    <fx:include source="systemView.fxml"/>
+    //    <fx:include source="filterView.fxml"/>
+}
