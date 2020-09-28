e788f659ae9b6fd62893f8f5539a1df6bbf415a1
Milan Todorovic <todor-90@hotmail.com>
2020-09-28 18:32:28 +0200

    Experimenting with layout

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,33 +1,39 @@
 package core;
 
+import core.layout.MainSceneController;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("layout/mainScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("layout/mainScene.fxml"));
+        Parent root = fxmlLoader.load();
+        MainSceneController controller = fxmlLoader.getController();
+
         primaryStage.setTitle("Colonization and Exploration Assistant v0.1");
 
         Scene mainScene = new Scene(root, 300, 300);
 
-        Pane rootPane = new Pane();
-
-        Pane mapView = new Pane();
-        Pane systemView = new Pane();
-        Pane planetView = new Pane();
-        Pane filterView = new Pane();
-
-        rootPane.getChildren().addAll(mapView,systemView,planetView, filterView);
-
         primaryStage.setScene(mainScene);
         primaryStage.show();
+
+//        controller.canvas.draw();
+
     }
 
     public static void main(String[] args) {
--- src/core/layout/MainSceneController.java
+++ src/core/layout/MainSceneController.java
@@ -1,18 +1,72 @@
 package core.layout;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class MainSceneController {
+public class MainSceneController implements Initializable {
 
-    public Button btn;
+    @FXML
+    public AnchorPane parentPane;
+    @FXML
+    public TableView tableView;
 
-    public void handleButtonClick() {
-        btn.setText("Promenili smo tekst");
+    public static class ResizableCanvas extends Canvas{
+
+        public ResizableCanvas(double width, double height) {
+
+            setWidth(width);
+            setHeight(height);
+            widthProperty().addListener(evt -> draw());
+            heightProperty().addListener(evt -> draw());
+        }
+        // Redraw canvas when size changes.
+        public void draw() {
+
+//            GraphicsContext graphics_context = this.getGraphicsContext2D();
+//
+//            graphics_context.setFill(Color.DARKRED);
+//            for (int i=0; i<=500; i=i+50){
+//                graphics_context.fillOval(i, i, 50, 50);
+//            }
+
+        }
+
+        @Override
+        public boolean isResizable() {
+            return false;
+        }
     }
 
-    //    <fx:include source="planetView.fxml"/>
-    //    <fx:include source="mapView.fxml"/>
-    //    <fx:include source="systemView.fxml"/>
-    //    <fx:include source="filterView.fxml"/>
+    public ResizableCanvas canvas = new ResizableCanvas(200.0f, 200.0f);
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+//        parentPane.getChildren().add(canvas);
+//
+//        // Bind the width/height property to the wrapper Pane
+//        canvas.widthProperty().bind(parentPane.widthProperty());
+//        canvas.heightProperty().bind(parentPane.heightProperty());
+
+
+//        AnchorPane.setBottomAnchor(canvas , 8.0);
+//        AnchorPane.setRightAnchor(canvas , 5.0);
+//        AnchorPane.setTopAnchor(canvas , 8.0);
+//        AnchorPane.setLeftAnchor(canvas , 5.0);
+
+
+    }
 }
