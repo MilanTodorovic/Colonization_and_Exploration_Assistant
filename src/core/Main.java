b8d57d5677f32e5e074176c4871b83b208061af8
Milan Todorovic <todor-90@hotmail.com>
2020-10-01 15:42:33 +0200

    Added a check for type of operating system

--- src/core/Main.java
+++ src/core/Main.java
@@ -2,26 +2,16 @@ package core;
 
 import core.layout.MainSceneController;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.WatchEvent;
 
 public class Main extends Application {
 
     private static final String version = "v0.1";
+    private static final String OS_name = System.getProperty("os.name"); // name of the operating system
+
     // TODO make 2 more controllers for each VBox
     //  make one or more event listeners for clicks and selections (clicks on Nodes or toggles)
 
@@ -35,7 +25,8 @@ public class Main extends Application {
         Parent root = fxmlLoader.load();
         // TODO make a constructor in the Controller and pass a reference to the stage to call an alert window after an event
         MainSceneController controller = (MainSceneController) fxmlLoader.getController();
-        controller.menuBarController.setRoot(primaryStage);
+        controller.menuBarController.setRootStage(primaryStage);
+        controller.menuBarController.setOS_name(OS_name);
 
         // TODO add name of the save file to the title
         primaryStage.setTitle("Colonization and Exploration Assistant "+version);
